29a0c29e967f9da13082d785468a11774fd7f420
S-png61 <sagarbasiwal@gmail.com>
2026-02-23 23:24:23 +0530

    Add touch event listeners for mobile support

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ class Paper {
   rotating = false;
 
   init(paper) {
-    document.addEventListener('mousemove', (e) => {
+    document.addEventListener('touchmove', (e) => {
       if(!this.rotating) {
         this.mouseX = e.clientX;
         this.mouseY = e.clientY;
@@ -50,7 +50,7 @@ class Paper {
       }
     })
 
-    paper.addEventListener('mousedown', (e) => {
+    paper.addEventListener('touchstart', (e) => {
       if(this.holdingPaper) return; 
       this.holdingPaper = true;
       
